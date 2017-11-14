ec7583dca2d3183f89c3e65ea61c648069df3198
Alexander Olofsson <alexander.olofsson@liu.se>
2017-11-14 14:16:11 +0100

    Redo enum handling slightly
    Should now handle both arrays and hashes in a more proper manner

--- lib/fog/hyperv/fog_extensions/enum.rb
+++ lib/fog/hyperv/fog_extensions/enum.rb
@@ -8,22 +8,37 @@ module Fog
         super
       end
 
+      def ensure_value_getter
+        return if model.methods.include?("#{name}_values".to_sym)
+        model.class_eval <<-EOS, __FILE__, __LINE__
+            def #{name}_values
+              #{values}.freeze
+            end
+        EOS
+      end
+
       def create_setter
+        ensure_value_getter
         model.class_eval <<-EOS, __FILE__, __LINE__
             def #{name}=(new_#{name})
+              _values = #{name}_values
+              # FIXME: Prepare a key comparison array in advance
               if new_#{name}.is_a?(Fixnum)
-                if #{values}.is_a?(Array)
-                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not in the range (0..#{values.length - 1})" unless new_#{name} >= 0 && new_#{name} < #{values.length}
+                if _values.class.to_s == 'Array' # TODO: Better way to do class comparison in generated code
+                  puts "\#{_values} (\#{_values.class}) Is array"
+                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not in the range (0..\#{_values.length - 1})" unless new_#{name} >= 0 && new_#{name} < _values.length
                 else
-                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not one of #{values.is_a?(Hash) ? values.keys : values})" unless #{(values.is_a?(Hash) ? values.keys : values)}.include? new_#{name}
+                  puts "\#{_values} (\#{_values.class}) Is not array"
+                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not one of \#{_values.is_a?(Hash) ? _values.keys : _values})" unless (_values.is_a?(Hash) ? _values.keys : _values).include? new_#{name}
                 end
 
-                attributes[:#{name}] = #{values}[new_#{name}]
+                attributes[:#{name}] = _values[new_#{name}]
               elsif new_#{name}.nil?
                 attributes[:#{name}] = nil
               else
+                puts "Is string"
                 new_#{name} = new_#{name}.to_s.to_sym unless new_#{name}.is_a? String
-                raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not one of #{values.is_a?(Hash) ? values.keys : values})" unless #{(values.is_a?(Hash) ? values.keys : values)}.include? new_#{name}
+                raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not one of \#{_values.is_a?(Hash) ? _values.keys : _values})" unless (_values.is_a?(Hash) ? _values.keys : _values).include? new_#{name}
                 attributes[:#{name}] = new_#{name}
               end
             end
@@ -31,22 +46,20 @@ module Fog
       end
 
       def create_getter
+        ensure_value_getter
         model.class_eval <<-EOS, __FILE__, __LINE__
             def #{name}_num
+              _values = #{name}_values
               return nil if self.#{name}.nil?
               if self.#{name}.is_a?(Fixnum)
                 self.#{name}
               else
-                if #{values}.is_a?(Hash)
-                  #{values}.key(self.#{name})
+                if _values.is_a?(Hash)
+                  _values.key(self.#{name})
                 else
-                  #{values}.index(self.#{name})
+                  _values.index(self.#{name})
                 end
               end
-            end
-
-            def #{name}_values
-              #{values}.freeze
             end
         EOS
         super
