4d39d4fb1f1b3aa8cb4eff053d98374d05ffe0b8
Alexander Olofsson <alexander.olofsson@liu.se>
2018-01-24 16:53:49 +0100

    Support Ruby 2.4 better

--- lib/fog/hyperv/fog_extensions/enum.rb
+++ lib/fog/hyperv/fog_extensions/enum.rb
@@ -23,19 +23,19 @@ module Fog
             def #{name}=(new_#{name})
               _values = #{name}_values
               # FIXME: Prepare a key comparison array in advance
-              if new_#{name}.is_a?(Fixnum)
+              if new_#{name}.is_a?(Integer)
                 if _values.class.to_s == 'Array' # TODO: Better way to do class comparison in generated code
                   raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not in the range (0..\#{_values.length - 1})" unless new_#{name} >= 0 && new_#{name} < _values.length
                   attributes[:#{name}] = _values[new_#{name}]
                 else
-                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not one of \#{_values.is_a?(Hash) ? _values.values : _values})" unless (_values.is_a?(Hash) ? _values.values : _values).include? new_#{name}
+                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}.inspect} is not one of \#{_values.is_a?(Hash) ? _values.values : _values})" unless (_values.is_a?(Hash) ? _values.values : _values).include? new_#{name}
                   attributes[:#{name}] = _values.key(new_#{name})
                 end
               elsif new_#{name}.nil?
                 attributes[:#{name}] = nil
               else
                 new_#{name} = new_#{name}.to_s.to_sym unless new_#{name}.is_a? Symbol
-                raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not one of \#{_values.is_a?(Hash) ? _values.keys : _values})" unless (_values.is_a?(Hash) ? _values.keys : _values).include? new_#{name}
+                raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}.inspect} is not one of \#{_values.is_a?(Hash) ? _values.keys : _values})" unless (_values.is_a?(Hash) ? _values.keys : _values).include? new_#{name}
                 attributes[:#{name}] = new_#{name}
               end
             end
