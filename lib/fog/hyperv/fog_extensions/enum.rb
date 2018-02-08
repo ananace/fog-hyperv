6be093ec0196e78585907abbdf31f689320eea95
Alexander "Ace" Olofsson <ace@haxalot.com>
2018-02-08 21:32:12 +0100

    Continuing to clean up enum handling

--- lib/fog/hyperv/fog_extensions/enum.rb
+++ lib/fog/hyperv/fog_extensions/enum.rb
@@ -5,12 +5,21 @@ module Fog
 
       def initialize(model, name, options)
         @values = options.fetch(:values, [])
+
+        raise Fog::Hyperv::Errors::ServiceError, "#{values} is not a valid array or hash" \
+          unless values.is_a?(Array) || \
+            (values.is_a?(Hash) && \
+             values.keys.all? { |k| k.is_a? Symbol } && \
+             values.values.all? { |v| v.is_a? Numeric })
+
         super
       end
 
       def ensure_value_getter
-        return if model.methods.include?("#{name}_values".to_sym)
+        return if model.private_methods.include?("#{name}_values".to_sym)
+
         model.class_eval <<-EOS, __FILE__, __LINE__
+            private
             def #{name}_values
               #{values}.freeze
             end
@@ -19,24 +28,31 @@ module Fog
 
       def create_setter
         ensure_value_getter
+
+        # Add a setter that always stores a symbol value
         model.class_eval <<-EOS, __FILE__, __LINE__
-            def #{name}=(new_#{name})
+            def #{name}=(new_value)
               _values = #{name}_values
-              # FIXME: Prepare a key comparison array in advance
-              if new_#{name}.is_a?(Numeric)
-                if _values.class.to_s == 'Array' # TODO: Better way to do class comparison in generated code
-                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}} is not in the range (0..\#{_values.length - 1})" unless new_#{name} >= 0 && new_#{name} < _values.length
-                  attributes[:#{name}] = _values[new_#{name}]
-                else
-                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}.inspect} is not one of \#{_values.is_a?(Hash) ? _values.values : _values})" unless (_values.is_a?(Hash) ? _values.values : _values).include? new_#{name}
-                  attributes[:#{name}] = _values.key(new_#{name})
+              if new_value.is_a?(Numeric)
+                # TODO: Better way to do class comparison in generated code
+                if _values.class.to_s == 'Array'
+                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_value} is not in the range (0..\#{_values.length - 1})" \
+                    unless new_value >= 0 && new_value < _values.length
+                  attributes[:#{name}] = _values[new_value]
+                elsif _values.class.to_s == 'Hash'
+                  raise Fog::Hyperv::Errors::ServiceError, "\#{new_value} is not one of \#{_values.values})" \
+                    unless _values.values.include? new_value
+                  attributes[:#{name}] = _values.key(new_value)
                 end
-              elsif new_#{name}.nil?
+              elsif new_value.nil?
                 attributes[:#{name}] = nil
               else
-                new_#{name} = new_#{name}.to_s.to_sym unless new_#{name}.is_a? Symbol
-                raise Fog::Hyperv::Errors::ServiceError, "\#{new_#{name}.inspect} is not one of \#{_values.is_a?(Hash) ? _values.keys : _values})" unless (_values.is_a?(Hash) ? _values.keys : _values).include? new_#{name}
-                attributes[:#{name}] = new_#{name}
+                new_value = new_value.to_s.to_sym unless new_value.is_a? Symbol
+                # Ensure values is the array of enum symbols
+                _values = (_values.is_a?(Hash) ? _values.keys : _values)
+                raise Fog::Hyperv::Errors::ServiceError, "\#{new_value.inspect} is not one of \#{_values})" \
+                  unless _values.include? new_value
+                attributes[:#{name}] = new_value
               end
             end
         EOS
@@ -44,23 +60,27 @@ module Fog
 
       def create_getter
         ensure_value_getter
+
+        # Add a getter for <enum>_num to get the numeric value
         model.class_eval <<-EOS, __FILE__, __LINE__
             def #{name}_num
               _values = #{name}_values
-              _attrib = attributes[:#{name}]
+              _value = attributes[:#{name}]
 
-              return nil if _attrib.nil?
-              if _attrib.is_a?(Numeric)
-                _attrib
+              return nil if _value.nil?
+              if _value.is_a?(Numeric)
+                _value
               else
                 if _values.is_a?(Hash)
-                  _values.send(:[],_attrib)
+                  _values.send(:[], _value)
                 else
-                  _values.index(_attrib)
+                  _values.index(_value)
                 end
               end
             end
         EOS
+        
+        # Add the default getter for the symbol value
         super
       end
     end
